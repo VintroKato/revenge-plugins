582719483378632e0c843b2162ee680b2c52211d
Vintro <125207680+VintroKato@users.noreply.github.com>
2025-08-15 21:39:34 +0200

    Update index.js

--- template/index.js
+++ template/index.js
@@ -1,1 +1,25 @@
-(function(e,o,n){"use strict";const{FormText:t}=n.Forms;function l(){return React.createElement(t,null,"Hello, world!")}var r={onLoad:function(){o.logger.log("Hello world!")},onUnload:function(){o.logger.log("Goodbye, world.")},settings:l};return e.default=r,Object.defineProperty(e,"__esModule",{value:!0}),e})({},vendetta,vendetta.ui.components);
+import { before } from "@vendetta/patcher";
+import { findByDisplayName } from "@vendetta/metro";
+import { LayoutAnimation, UIManager, Platform } from "react-native";
+
+let patch;
+
+export default {
+    onLoad: () => {
+        if (Platform.OS === "android" && UIManager.setLayoutAnimationEnabledExperimental) {
+            UIManager.setLayoutAnimationEnabledExperimental(true);
+        }
+        const MessageList = findByDisplayName("MessageList");
+        if (!MessageList) return;
+
+        patch = before("componentDidUpdate", MessageList.prototype, function (prevProps) {
+            if (prevProps?.messages?.length !== this.props?.messages?.length) {
+                LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+            }
+        });
+    },
+
+    onUnload: () => {
+        if (patch) patch();
+    },
+};
